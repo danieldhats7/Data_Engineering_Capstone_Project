638245fa6a560ce13352f541de8df6d5630fb531
Daniel Reyes <danieldhatsct@gmail.com>
2022-04-04 20:49:00 -0500

    create DDL to define table

---
+++ sql/sales_data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE sales_data (
+    product_id Integer NOT NULL,
+	customer_id Integer NOT NULL,
+    price Integer NOT NULL,
+    quantity Integer NOT NULL,
+    timestamp TIMESTAMP NOT NULL
+);
